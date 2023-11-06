09696f0fd0ec28da827344c24ca22dee92720ff0
Thierry Moudiki <thierry.moudiki@gmail.com>
2023-11-06 15:10:51 +0100

    use virtual env

--- R/zzz.R
+++ R/zzz.R
@@ -5,21 +5,37 @@ sklearn <- NULL
 .onLoad <- function(libname, pkgname) {
   utils::install.packages("reticulate",
                           repos = list(CRAN = "https://cloud.r-project.org"))
-  # try(reticulate::virtualenv_create('./r-reticulate'),
-  #     silent = TRUE)
-  # try(reticulate::use_virtualenv('./r-reticulate'),
-  #     silent = TRUE)
+  try(reticulate::virtualenv_create('./r-reticulate'),
+      silent = TRUE)
+  try(reticulate::use_virtualenv('./r-reticulate'),
+      silent = TRUE)
   try(reticulate::py_install(
     "Cython",
-    #envname = "r-reticulate",
+    envname = "./r-reticulate",
     pip = TRUE,
     pip_options = "--upgrade",
     pip_ignore_installed = TRUE
   ),
   silent = TRUE)
+  try(reticulate::py_install("numpy",
+                             envname = "./r-reticulate",
+                             pip = TRUE),
+      silent = TRUE)
+  try(reticulate::py_install("pandas",
+                             envname = "./r-reticulate",
+                             pip = TRUE),
+      silent = TRUE)
+  try(reticulate::py_install("scipy",
+                             envname = "./r-reticulate",
+                             pip = TRUE, ),
+      silent = TRUE)
+  try(reticulate::py_install("scikit-learn",
+                             envname = "./r-reticulate",
+                             pip = TRUE),
+      silent = TRUE)
   try(reticulate::py_install(
     "scikit-learn",
-    #envname = "r-reticulate",
+    envname = "./r-reticulate",
     pip = TRUE,
     pip_options = "--upgrade",
     pip_ignore_installed = TRUE
@@ -27,7 +43,7 @@ sklearn <- NULL
   silent = TRUE)
   try(reticulate::py_install(
     "mlsauce",
-    #envname = "r-reticulate",
+    envname = "./r-reticulate",
     pip = TRUE,
     pip_options = "--upgrade",
     pip_ignore_installed = TRUE
