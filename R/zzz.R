5aaf004a3e6b0958bbe41d2398031c73152fc6fe
Thierry Moudiki <thierry.moudiki@gmail.com>
2023-11-06 10:08:19 +0100

    try to not use virtual env

--- R/zzz.R
+++ R/zzz.R
@@ -1,29 +1,24 @@
 # global reference to scipy (will be initialized in .onLoad)
 ms <- NULL
+sklearn <- NULL
 
 .onLoad <- function(libname, pkgname) {
-
-  if(is_package_available("reticulate") == FALSE)
-  {
-    install.packages("reticulate",
-                     repos = c(CRAN="https://cloud.r-project.org"))
-  }
-
-  require("reticulate")
-
-  try(reticulate::virtualenv_create('./r-reticulate',
-                                    packages = c('numpy',
-                                                 'scipy',
-                                                 'Cython')),
-      silent = TRUE)
-
-  try(reticulate::use_virtualenv('./r-reticulate'),
-      silent = TRUE)
-
-  reticulate::virtualenv_install(packages = "git+https://github.com/Techtonique/mlsauce.git",
-                                 envname = "./r-reticulate",
-                                 pip_options = "--upgrade")
-
+  utils::install.packages("reticulate",
+                          repos = list(CRAN = "https://cloud.r-project.org"))
+  # try(reticulate::virtualenv_create('./r-reticulate'),
+  #     silent = TRUE)
+  # try(reticulate::use_virtualenv('./r-reticulate'),
+  #     silent = TRUE)
+  try(reticulate::py_install(
+    "mlsauce",
+    #envname = "r-reticulate",
+    pip = TRUE,
+    pip_options = "--upgrade",
+    pip_ignore_installed = TRUE
+  ),
+  silent = TRUE)
   # use superassignment to update global reference to package
   ms <<- reticulate::import("mlsauce", delay_load = TRUE)
+  sklearn <<- reticulate::import("sklearn", delay_load = TRUE)
 }
+.onLoad <- memoise::memoise(.onLoad)
