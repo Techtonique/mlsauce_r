415c4d37c7f08b8b256625f7dcd4777c1490b78f
Thierry Moudiki <thierry.moudiki@gmail.com>
2023-11-19 17:29:20 +0100

    dowload data

--- R/download.R
+++ R/download.R
@@ -1,12 +1,13 @@
 
 
+
 #' Download datasets
 #'
-#' @param pkgname a string; R package name 
-#' @param dataset a string; dataset name 
+#' @param pkgname a string; R package name
+#' @param dataset a string; dataset name
 #' @param source a string; package location (address)
 #'
-#' @return A data frame 
+#' @return A data frame
 #' @export
 #'
 #' @examples
@@ -14,7 +15,14 @@
 #' df <- mlsauce::download(pkgname="MASS", dataset="Boston", source="https://cran.r-universe.dev/")
 #' print(df)
 #'
-download <- function(pkgname="MASS", dataset="Boston", source="https://cran.r-universe.dev/")
-{
- ms$download(pkgname=pkgname, dataset=dataset, source=source)
-}
+download <-
+  function(pkgname = "MASS",
+           dataset = "Boston",
+           source = "https://cran.r-universe.dev/")
+  {
+    as.data.frame(mlsauce::ms$download(
+      pkgname = pkgname,
+      dataset = dataset,
+      source = source
+    ))
+  }
