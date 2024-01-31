ae7ce502b2a9ae1ea4ec7acec51988bbd79cc4f2
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-01-31 17:38:49 +0100

    try different envs

--- R/zzz.R
+++ R/zzz.R
@@ -5,53 +5,48 @@ sklearn <- NULL
 .onLoad <- function(libname, pkgname) {
   utils::install.packages("reticulate",
                           repos = list(CRAN = "https://cloud.r-project.org"))
-
-  try(reticulate::virtualenv_create('./r-reticulate'), silent = TRUE)
-
-  try(reticulate::use_virtualenv('./r-reticulate'), silent = TRUE)
-
-  try(reticulate::py_install(
-    "Cython",
-    envname = "./r-reticulate",
-    pip = TRUE,
-    pip_options = "--upgrade",
-    pip_ignore_installed = TRUE), silent = TRUE)
-
-  try(reticulate::py_install("numpy",
-                             envname = "./r-reticulate",
-                             pip = TRUE), silent = TRUE)
-
-  try(reticulate::py_install("pandas",
-                             envname = "./r-reticulate",
-                             pip = TRUE), silent = TRUE)
-
-  try(reticulate::py_install("requests",
-                             envname = "./r-reticulate",
-                             pip = TRUE), silent = TRUE)                           
-
-  try(reticulate::py_install("scipy",
-                             envname = "./r-reticulate",
-                             pip = TRUE), silent = TRUE)
-
-  try(reticulate::py_install("scikit-learn",
-                             envname = "./r-reticulate",
-                             pip = TRUE), silent = TRUE)
-
-  try(reticulate::py_install(
-    "scikit-learn",
-    envname = "./r-reticulate",
-    pip = TRUE,
-    pip_options = "--upgrade",
-    pip_ignore_installed = TRUE
-  ), silent = TRUE)
-
-  try(reticulate::py_install(
-    "mlsauce",
-    envname = "./r-reticulate",
-    pip = TRUE,
-    pip_options = c("--upgrade", "--verbose"),
-    pip_ignore_installed = TRUE
-  ), silent = TRUE)
+  
+  envs <- c("./r-reticulate", ".", NULL)
+  lapply(envs, function(env)
+  {
+    try(reticulate::virtualenv_create(env), silent = TRUE)
+    try(reticulate::use_virtualenv(env), silent = TRUE)
+    try(reticulate::py_install(
+      "Cython",
+      envname = env,
+      pip = TRUE,
+      pip_options = "--upgrade",
+      pip_ignore_installed = TRUE), silent = TRUE)
+    try(reticulate::py_install("numpy",
+                              envname = env,
+                              pip = TRUE), silent = TRUE)
+    try(reticulate::py_install("pandas",
+                              envname = env,
+                              pip = TRUE), silent = TRUE)
+    try(reticulate::py_install("requests",
+                              envname = env,
+                              pip = TRUE), silent = TRUE)                           
+    try(reticulate::py_install("scipy",
+                              envname = env,
+                              pip = TRUE), silent = TRUE)
+    try(reticulate::py_install("scikit-learn",
+                              envname = env,
+                              pip = TRUE), silent = TRUE)
+    try(reticulate::py_install(
+      "scikit-learn",
+      envname = env,
+      pip = TRUE,
+      pip_options = "--upgrade",
+      pip_ignore_installed = TRUE
+    ), silent = TRUE)
+    try(reticulate::py_install(
+      "mlsauce",
+      envname = env,
+      pip = TRUE,
+      pip_options = c("--upgrade", "--verbose"),
+      pip_ignore_installed = TRUE
+    ), silent = TRUE)
+  })
 
   # use superassignment to update global reference to package
   ms <<- reticulate::import("mlsauce", delay_load = TRUE)
